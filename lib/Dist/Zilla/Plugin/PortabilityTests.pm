755f23a0a8273617686313d174a5dc58b71f94f6
Randy Stauner <randy@magnificent-tears.com>
2011-06-21 20:13:06 -0700

    Specify test options with 'options' attribute

--- lib/Dist/Zilla/Plugin/PortabilityTests.pm
+++ lib/Dist/Zilla/Plugin/PortabilityTests.pm
@@ -6,6 +6,33 @@ package Dist::Zilla::Plugin::PortabilityTests;
 # ABSTRACT: Release tests for portability
 use Moose;
 extends 'Dist::Zilla::Plugin::InlineFiles';
+with 'Dist::Zilla::Role::FileMunger';
+
+has options => (
+  is      => 'ro',
+  isa     => 'Str',
+  default => '',
+);
+
+sub munge_file {
+  my ($self, $file) = @_;
+  return unless $file->name eq 'xt/release/portability.t';
+
+  # 'name => val, name=val'
+  my %options = split(/\W+/, $self->options);
+
+  if ( keys %options ) {
+    my $content = $file->content;
+
+    my $optstr = join ', ', map { "$_ => $options{$_}" } sort keys %options;
+
+    # insert options() above run_tests;
+    $content =~ s/^(run_tests\(\);)$/options($optstr);\n$1/m;
+
+    $file->content($content);
+  }
+  return;
+}
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
@@ -24,6 +51,8 @@ __END__
 In C<dist.ini>:
 
     [PortabilityTests]
+    ; you can optionally specify test options
+    options = test_dos_length = 1, use_file_find = 0
 
 =head1 DESCRIPTION
 
@@ -31,6 +60,11 @@ This is an extension of L<Dist::Zilla::Plugin::InlineFiles>, providing the
 following file:
 
   xt/release/portability.t - a standard Test::Portability::Files test
+
+You can set options for the tests in the 'options' attribute:
+Specify C<< name = value >> separated by commas.
+
+See L<Test::Portability::Files/options> for possible options.
 
 =cut
 
